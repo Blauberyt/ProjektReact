7e609cb2d0475c13fe3b16bba31551cf2bb4c4ac
Piotr Sobolewski <149850731+Blauberyt@users.noreply.github.com>
2024-06-22 12:37:07 +0200

    Replace favicon

--- src/Searchbar.js
+++ src/Searchbar.js
@@ -4,12 +4,15 @@ import { IconButton } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import { useState, useEffect } from "react";
 
+const APP_ID = 'c8580dfa';
+const APP_KEY = '3c7100a6a70e7841d5a938249a544a5b';
+
 export const Searchbar = ({ setRecipes }) => {
-  const [query, setQuery] = useState("chicken");
+  const [query, setQuery] = useState(   );
   console.log(query);
   async function fetchRecipes() {
     const response = await fetch(
-      `https://api.edamam.com/api/recipes/v2?q=${query}&type=public`,
+      `https://api.edamam.com/api/recipes/v2?q=${query}&type=public&app_id=${APP_ID}&app_key=${APP_KEY}`,
     );
     const data = await response.json();
     setRecipes(data.hits);
